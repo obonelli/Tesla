0377211225d3482474459fd465518c947c632a43
obonelli <bonelli.personal@gmail.com>
2021-03-07 17:52:48 -0600

    Header v1 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
+import React from 'react'
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Clever programmer</h1>
+      <Header />
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
     </div>
   );
 }
